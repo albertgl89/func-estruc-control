b97f2ad6479de4c100350e1f47ed97d18c5ad115
Albert Garcia Llorca <albertgarcia20@gmail.com>
2021-11-09 10:24:20 +0000

    Nivell 1 / Exercici 2 finalitzat

--- nivell-1/index.php
+++ nivell-1/index.php
@@ -20,3 +20,19 @@ echo "El número 6 és ";
 echo parellOImparell(6)? "parell.<br>" : "imparell.<br>";
 echo "El número 9 és ";
 echo parellOImparell(9)? "parell.<br>" : "imparell.<br>";
+
+echo "<h1> Exercici 2 </h1>";
+
+function comptaTrampos(){
+    for ($i = 1; $i < 10; $i++){
+        $i++;
+        if ($i == 10){
+            echo $i;
+        } else {
+            echo $i.", ";
+        } 
+    }
+}
+
+//Test
+comptaTrampos();
